b2061cf08367b05e58336c56a0b84cd1b550eed4
sp0ozy <imolchanov210@gmail.com>
2026-02-09 14:30:56 +0100

    adjustments in constants

--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -1,10 +1,7 @@
 import os
-import sys
-import numpy as np
-import pandas as pd
 
 """
-defining common constant variable for trainning pipeline
+Defining common constants variable for training pipeline
 """
 
 TARGET_COLUMN:str = "Result"
@@ -13,6 +10,11 @@ ARTIFACT_DIR:str = "artifacts"
 FILE_NAME:str = "NetworkData.csv"
 TRAIN_FILE_NAME:str = "train.csv"
 TEST_FILE_NAME:str = "test.csv"
+SCHEMA_FILE_PATH:str = os.path.join("data_schema","schema.yaml")
+
+"""
+Defining common constant variable for data ingestion
+"""
 
 DATA_INGESTION_COLLECTION_NAME:str = "NetworkData"
 DATA_INGESTION_DATABASE_NAME:str = "NetworkSecurity"
@@ -20,6 +22,10 @@ DATA_INGESTION_DIR_NAME:str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR:str = "feature_store"
 DATA_INGESTION_INGESTED_DIR:str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO:float = 0.2
+
+"""
+Defining common constant variable for data validation
+"""
 
 DATA_VALIDATION_DIR_NAME:str = "data_validation"
 DATA_VALIDATION_VALID_DIR:str = "validated"
