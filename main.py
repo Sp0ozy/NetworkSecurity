7c28faf11e9ab641a17c5da3baaa71dd6ba950bf
sp0ozy <imolchanov210@gmail.com>
2026-02-09 17:02:54 +0100

    data validation

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from networksecurity.components.data_ingestion import DataIngestion
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig
 from networksecurity.exceptions.exception import NetworkSecurityException
+
 import sys
 
 if __name__ == "__main__":
     try:
-        data_ingestion_config = DataIngestionConfig(training_pipeline_config=TrainingPipelineConfig())
+        training_pipeline_config=TrainingPipelineConfig()
+        data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
         data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
         print("Starting data ingestion process")
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
         print(f"Data Ingestion completed successfully. Data Ingestion Artifact: {data_ingestion_artifact}")
+
+        data_validation_config = DataValidationConfig(training_pipeline_config=training_pipeline_config)
+        data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                         data_validation_config=data_validation_config)
+        print("Starting data validation process")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        print(f"Data Validation completed successfully. Data Validation Artifact: {data_validation_artifact}")
+        
     except Exception as e:
         raise NetworkSecurityException(e,sys) from e
--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -2,7 +2,7 @@ from networksecurity.entity.artifact_entity import DataIngestionArtifact, DataVa
 from networksecurity.entity.config_entity import DataValidationConfig
 from networksecurity.exceptions.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.utils.main_utils import read_yaml_file
+from networksecurity.utils.main_utils import read_yaml_file, write_yaml_file
 from networksecurity.constants.training_pipeline import SCHEMA_FILE_PATH
 
 from scipy.stats import ks_2samp
@@ -27,7 +27,38 @@ class DataValidation:
     
     def validate_number_of_columns(self, dataframe: pd.DataFrame) -> bool:
         try:
-            pass
+            number_of_columns = len(self._schema_config['columns'])
+            logging.info(f"Required number of columns: {number_of_columns}")
+            logging.info(f"Dataframe has columns: {dataframe.columns}")
+            if len(dataframe.columns) == number_of_columns:
+                return True
+            return False
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    def detect_data_drif(self, base_df, current_df, threshold=0.05) -> bool:
+        try:
+            status=True
+            report = {}
+            for column in base_df.columns:
+                d1=base_df[column]
+                d2=current_df[column]
+                is_same_dist = ks_2samp(d1, d2)
+                if is_same_dist.pvalue > threshold:
+                    is_Found = False
+                else:
+                    is_Found = True
+                    status=False
+                report.update({column:{
+                    "p_value": float(is_same_dist.pvalue),
+                    "drift_status": is_Found
+                }})
+
+            drift_report_file_path = self.data_validation_config.drift_report_file_path
+            os.makedirs(os.path.dirname(drift_report_file_path), exist_ok=True)
+            write_yaml_file(file_path=drift_report_file_path, content=report)
+            return status
+
         except Exception as e:
             raise NetworkSecurityException(e, sys)
     
@@ -39,9 +70,61 @@ class DataValidation:
             train_df = DataValidation.read_data(trained_file_path)
             test_df = DataValidation.read_data(test_file_path)
 
+            # validate number of columns
+            error_messages = []
+            train_schema_status = self.validate_number_of_columns(train_df)
+            if not train_schema_status:
+                error_messages.append("Train dataframe does not have the required number of columns.\n")
+                self.data_validation_config.valid_train_file_path=None
+            test_schema_status = self.validate_number_of_columns(test_df)
+            if not test_schema_status:
+                self.data_validation_config.valid_test_file_path=None
+                error_messages.append("Test dataframe does not have the required number of columns.\n")
 
+            #checking data drift
+            train_drift_status = self.detect_data_drif(base_df=train_df, current_df=test_df)
+            if not train_drift_status:
+                self.data_validation_config.valid_train_file_path=None
+                error_messages.append("Data drift detected between train and test data.\n")
+                
+            test_drift_status = self.detect_data_drif(base_df=test_df, current_df=test_df)
+            if not test_drift_status:
+                self.data_validation_config.valid_test_file_path=None
+                error_messages.append("Data drift detected in test data.\n")
 
+            if  train_schema_status and train_drift_status:
+                train_output_file_path =self.data_validation_config.valid_train_file_path
+                os.makedirs(os.path.dirname(train_output_file_path), exist_ok=True)
+                train_df.to_csv(train_output_file_path, index=False)
+                self.data_validation_config.invalid_train_file_path=None
+            else:
+                train_output_file_path =self.data_validation_config.invalid_train_file_path
+                os.makedirs(os.path.dirname(train_output_file_path), exist_ok=True)
+                train_df.to_csv(train_output_file_path, index=False)
+                self.data_validation_config.invalid_train_file_path=None
 
+            if  test_schema_status and test_drift_status:
+                test_output_file_path =self.data_validation_config.valid_test_file_path
+                os.makedirs(os.path.dirname(test_output_file_path), exist_ok=True)
+                test_df.to_csv(test_output_file_path, index=False)
+                self.data_validation_config.invalid_test_file_path=None
+            else:
+                test_output_file_path =self.data_validation_config.invalid_test_file_path
+                os.makedirs(os.path.dirname(test_output_file_path), exist_ok=True)
+                test_df.to_csv(test_output_file_path, index=False)
+                self.data_validation_config.invalid_test_file_path=None
+            
+            if len(error_messages) > 0:
+                raise Exception(f"Data Validation Error: {'\n'.join(error_messages)}")
 
+            return DataValidationArtifact(
+                validation_status = train_schema_status and test_schema_status and train_drift_status and test_drift_status,
+                valid_train_file_path=self.data_validation_config.valid_train_file_path,
+                valid_test_file_path=self.data_validation_config.valid_test_file_path,
+                invalid_train_file_path=self.data_validation_config.invalid_train_file_path,
+                invalid_test_file_path=self.data_validation_config.invalid_test_file_path,
+                drift_report_file_path=self.data_validation_config.drift_report_file_path,
+                
+            )
         except Exception as e:
             raise NetworkSecurityException(e, sys) from e
