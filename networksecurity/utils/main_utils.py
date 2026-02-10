bf819225cfd5f59e6f948bfbfab6fe9f1bde1eaa
sp0ozy <imolchanov210@gmail.com>
2026-02-10 22:05:12 +0100

    save_numpy and save_object added

--- networksecurity/utils/main_utils.py
+++ networksecurity/utils/main_utils.py
@@ -1,8 +1,9 @@
-import re
+import numpy as np
 from networksecurity.exceptions.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 import os, sys
 import yaml
+import pickle
 
 def read_yaml_file(file_path: str) -> dict:
     """
@@ -25,5 +26,27 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
         os.makedirs(os.path.dirname(file_path), exist_ok=True)     
         with open(file_path, 'w') as file:
             yaml.dump(content, file)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+    
+def save_numpy_array_data(file_path: str, array: np.array) -> None:
+    """
+    Saves a numpy array to a file.
+    """
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, 'wb') as file:
+            np.save(file, array)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+    
+def save_object(file_path: str, obj: object) -> None:
+    """
+    Saves a Python object to a file using pickle.
+    """
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, 'wb') as file:
+            pickle.dump(obj, file)
     except Exception as e:
         raise NetworkSecurityException(e, sys)
