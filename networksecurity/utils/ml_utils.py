32b8880951b0586d73161f87b91ac2fd67e75864
sp0ozy <imolchanov210@gmail.com>
2026-02-14 23:52:43 +0200

    evaluate models added

--- networksecurity/utils/ml_utils.py
+++ networksecurity/utils/ml_utils.py
@@ -1,5 +1,8 @@
 import os
 import sys
+
+from sklearn.base import r2_score
+from sklearn.model_selection import GridSearchCV
 from networksecurity.entity.artifact_entity import ClassificationMetricArtifact
 from networksecurity.exceptions.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging  
@@ -22,6 +25,33 @@ def get_classification_score(y_true, y_pred) -> ClassificationMetricArtifact:
         )
     except Exception as e:
         raise NetworkSecurityException(e, sys)
+
+def evaluate_models(X_train, y_train, X_test, y_test, params, models: dict) -> dict:
+    """
+    Evaluates multiple classification models and returns their performance metrics.
+    """
+    try:
+        report={}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            params = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, params, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+        return report
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
     
 class NetworkModel:
     def __init__(self, preprocessor, model):
@@ -38,3 +68,4 @@ class NetworkModel:
             return y_hat
         except Exception as e:
             raise NetworkSecurityException(e, sys)
+
